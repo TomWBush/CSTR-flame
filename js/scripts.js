4f947d7e92dc0ee0b21d8c2ade5e1a5b6e56e4a0
zww <zww@umich.edu>
2020-07-10 20:44:11 -0400

    Add animation colors
    - Draw color from elements in gradient

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@ function changeColorHelper(color, time) {
     setTimeout(function() {
         let innerFlame = document.getElementsByClassName("inner flame")[0];
         innerFlame.setAttribute("style", "background-color: " + color + "; box-shadow: 0px 0px 9px 4px " + color + ";");
-    }, time);
+	}, time);
 }
 
 function changeColor() {
-    changeColorHelper("#00e600", 1500);
-    changeColorHelper("#6495ED", 3000);
+	for (let i = 0; i < gradient.length; i++) {
+		changeColorHelper(gradient[i], (i + 1) * 100);
+	}
 }
 
 function changeTemp() {
 	let innerFlame = document.getElementsByClassName("inner flame")[0];
-	let val = document.getElementById("tempRange").value;
+	let val = document.getElementById("temp-range").value;
 	var color = gradient[val];
 	innerFlame.setAttribute("style", "background-color: " + color + "; box-shadow: 0px 0px 9px 4px " + color + ";");
 }
